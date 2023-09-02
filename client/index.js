be056089ade712dc5efdf1c64a5650dfec556447
Nathan Doucette <fluffyandnathan@gmail.com>
2023-09-02 13:57:31 -0400

    got react flow to render

--- client/index.js
+++ client/index.js
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
-
+// import 'reactflow/dist/base.css';
+// import 'reactflow/dist/style.css';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
