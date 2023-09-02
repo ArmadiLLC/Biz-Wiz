a895c27c3d72267563c1ae96b7887418611376e0
jacobsasser <140555768+jacobsasser@users.noreply.github.com>
2023-09-02 12:47:19 -0600

    Update loginRouter.js

--- server/routers/loginRouter.js
+++ server/routers/loginRouter.js
@@ -2,17 +2,16 @@ const express = require("express");
 
 //import controllers here:
 const loginController = require("../controllers/loginController.js");
-
+const sessionController = require("../controller/sessionController.js");
 
 const loginRouter = express.Router();
 
-// is sign on page the first thing? if so sign in redirects to sendFile(index.html)...
-loginRouter.get('/', loginController.signIn, (req, res) =>{
-    return res.status(200).json(res.locals.userId);
+loginRouter.get('/', loginController.signIn, sessionController.checkSession,  (req, res) =>{
+    return res.status(200).json();
 });
 
-loginRouter.post('/', loginController.SignUp, (req, res) =>{
-    return res.status(200).json(res.locals.userId);
+loginRouter.post('/', loginController.signUp, sessionController.createSession,  (req, res) =>{
+    return res.status(200).json(res.locals.cookie);
 })
 
 module.exports = loginRouter;
