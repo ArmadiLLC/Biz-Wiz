ed36b7393f7ccc9b203fd73f371588fd64b34f30
Akon530 <akondaveeti530@gmail.com>
2023-09-01 20:59:59 -0400

    set up react boilerplate

---
+++ client/actions/actions.js
@@ -0,0 +1,7 @@
+import * as types from '../constants/actionTypes';
+
+/*
+export const openDrawerActionCreator = currentView => ({
+    type: types.OPEN_DRAWER,
+    payload: currentView
+})*/
---
+++ client/reducers/chartsReducer.js
@@ -0,0 +1,14 @@
+import * as types from '../constants/actionTypes';
+
+const initialState = {
+    
+};
+  
+  const chartsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+  };
+  
+  export default chartsReducer;
