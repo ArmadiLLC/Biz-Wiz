a24a36a3f5e4cb2ec629b0f5f7a58323a639713b
Nathan Doucette <fluffyandnathan@gmail.com>
2023-09-02 11:42:51 -0400

    changed import to require

--- server/server.js
+++ server/server.js
@@ -1,40 +1,39 @@
-const patch = require("path");
-const express = rquire("express");
+const patch = require('path');
+const express = require('express');
 const app = express();
 // Optional Port for listening call
 const PORT = 3000;
 
 //import routers
-const apiRouter = require("./routers/apiRouter");
+const apiRouter = require('./routers/apiRouter');
 
 // Parse requests
 app.use(express.json());
 
 // Handle static assests - not sure where exactly we are requesting static files at this point
 // defaulting - /assets
-app.use("/assets", express.static("client/assets"));
+app.use('/assets', express.static('client/assets'));
 app.get('/dist', express.static('../dist'));
 
 // create routes for basic CRUD for getting/updating DATA
 // create route for logging in
 
-
 // homepage:
-app.get("/", (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, "/index.html"));
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '/index.html'));
 });
 // Go to routers for all other requests
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
 // default unknown route
-app.use((req, res) => res.status(404).send("ERROR: Could not find page!"));
+app.use((req, res) => res.status(404).send('ERROR: Could not find page!'));
 
 //default error handler
 app.use((err, req, res, next) => {
   const defaultError = {
-    log: "Express error handler caught unknown middleware error",
+    log: 'Express error handler caught unknown middleware error',
     status: 400,
-    message: { err: "An error occurred" },
+    message: { err: 'An error occurred' },
   };
   const errorObj = ({}, defaultError, err);
   // report to server what error occured
