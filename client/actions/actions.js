257c4807b34330d77a9f7ab31501dfe1dd13bc81
Akon530 <akondaveeti530@gmail.com>
2023-09-02 14:45:16 -0400

    added redux actions

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -5,3 +5,23 @@ export const openDrawerActionCreator = currentView => ({
     type: types.OPEN_DRAWER,
     payload: currentView
 })*/
+
+export const addEmployeeActionCreator = inputData => ({ // inputData = {firstName: , lastName: , jobTitle: , email: , bossID: , shortBio: , salary: }
+    type: types.ADD_EMPLOYEE,
+    payload: inputData
+})
+
+export const findEmployeeActionCreator = inputData => ({ //inputData = {element: , value: }
+    type: types.FIND_EMPLOYEE,
+    payload: inputData
+})
+
+export const updateEmployeeActionCreator = inputData => ({ //inputData = {employeeID: , newEmployeeObject: }
+    type: types.UPDATE_EMPLOYEE,
+    payload: inputData
+})
+
+export const deleteEmployeeActionCreator = inputData => ({ //inputData = {employeeID: }
+    type: types.DELETE_EMPLOYEE,
+    payload: inputData
+})
