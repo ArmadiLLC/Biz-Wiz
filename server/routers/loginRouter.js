d4a9c64da89deb22b61e90d3e12207de7afe393e
Nathan Doucette <fluffyandnathan@gmail.com>
2023-09-02 16:24:36 -0400

    fixed some of the routing issues

--- server/routers/loginRouter.js
+++ server/routers/loginRouter.js
@@ -1,17 +1,27 @@
-const express = require("express");
+const express = require('express');
 
 //import controllers here:
-const loginController = require("../controllers/loginController.js");
-const sessionController = require("../controller/sessionController.js");
+const loginController = require('./../controllers/loginController');
+const sessionController = require('./../controllers/sessionController');
 
 const loginRouter = express.Router();
 
-loginRouter.get('/', loginController.signIn, sessionController.checkSession,  (req, res) =>{
+loginRouter.get(
+  '/',
+  loginController.signIn,
+  sessionController.checkSession,
+  (req, res) => {
     return res.status(200).json();
-});
+  },
+);
 
-loginRouter.post('/', loginController.signUp, sessionController.createSession,  (req, res) =>{
+loginRouter.post(
+  '/',
+  loginController.signUp,
+  sessionController.createSession,
+  (req, res) => {
     return res.status(200).json(res.locals.cookie);
-})
+  },
+);
 
 module.exports = loginRouter;
