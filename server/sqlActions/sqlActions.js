7e169620d97849da02ebd6fa6de7f06884d5e165
Nathan Doucette <fluffyandnathan@gmail.com>
2023-09-01 20:51:21 -0400

    added employee insertion and search

--- server/sqlActions/sqlActions.js
+++ server/sqlActions/sqlActions.js
@@ -7,14 +7,14 @@ const pool = new Pool({
 
 sqlActions = {};
 
-dbActions.createTables = async() => {
+sqlActions.createTables = async() => {
     try{
         await pool.query(`CREATE TABLE IF NOT EXISTS employees
         (id SERIAL PRIMARY KEY NOT NULL,
             firstname VARCHAR(50) NOT NULL
             lastname VARCHAR(50) NOT NULL
             jobtitle VARCHAR(50) NOT NULL
-            datehired TAMESTAMP
+            datehired TIMESTAMP
             email VARCHAR(50))
             bossid INTEGER
             shortbio VARCHAR(300)
@@ -33,4 +33,35 @@ dbActions.createTables = async() => {
     }catch(error){
         console.log(error.detail);
     }
+};
+
+sqlActions.createEmployee = async(data) =>{
+    const {firstname, lastname, jobtitle, datehired, email, shortbio, salary} = data;
+    const values = [firstname, lastname, jobtitle, datehired, email, shortbio, salary];
+    try{
+        const query = `INSERT into employees 
+        (firstname, lastname, jobtitle, datehired, email, shortbio, salary) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING id, firstname, lastname, jobtitle, datehired, email, bossid, shortbio, salary`;
+
+        const result = await pool.query(query, values);
+        return result.rows[0];
+    }catch(error){
+        console.log(error.detail);
+    }
+}
+sqlAction.searchEmployees = asyc(query) =>{
+    try{
+        const query = `SELECT * FROM employees WHERE `;
+        const values = [];
+        if(query.bossid !== undefined){
+            query += `bossid = $1`;
+            values.push(query.bossid);
+        }else{
+            query += `id = $1`;
+            values.push(query.employeeid);
+        }
+        const result = await pool.query(query, values);
+        return result.rows[0];
+    }catch(error){}
 }
